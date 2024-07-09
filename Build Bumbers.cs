275ba83c9a994b84576fc260151eed244bafd93d
UXP Controls Automated Porting System <uxpc@microsoft.com>
2024-07-09 15:37:54 -0400

    Initisl Commit

---
+++ Build Bumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BuildNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml("data.xml");
+            dataGridView1.DataSource = dataSet.Tables[0];           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Resources;
+
+namespace BuildNumbers
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        
+        static void Main()
+        {           
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+            DataGridView dataGridView1 = new DataGridView();
+            ContextMenu cm = new ContextMenu();
+            dataGridView1.ContextMenu = cm;
+            cm.MenuItems.Add("&Copy");
+            cm.Popup += Cm_Popup;
+        }
+
+        private static void Cm_Popup(object sender, EventArgs e)
+        {
+            DataGridView dataGridView1 = new DataGridView();            
+            //Clipboard clip1 = new Clipboard(dataGridView1);
+            //Clipboard.GetDataObject(dataGridView1.); 
+        }
+    }
+}
